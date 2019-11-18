d54411325af622a388ede2644a60658e70a609e5
kimiaki <kimioman704@gmail.com>
2019-11-18 22:04:19 +0900

    init xcode

---
+++ UISamplesProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UISamplesProject
+//
+//  Created by 鈴木公章 on 2019/11/18.
+//  Copyright © 2019 鈴木公章. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
